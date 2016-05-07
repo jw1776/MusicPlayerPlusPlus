d64e6a92e2ae3eef0d6810874e77947b44265aa0
Jack Westlake <taggardwarf@gmail.com>
2016-05-07 16:39:23 -0700

    Playlist Song Count
    -Added a TextView in the Playlists tab which displays the number of
    songs contained in each playlist

--- MusicPlayer++/app/src/main/java/group1/musicplayer/PlaylistAdapter.java
+++ MusicPlayer++/app/src/main/java/group1/musicplayer/PlaylistAdapter.java
@@ -47,8 +47,18 @@ public class PlaylistAdapter extends BaseAdapter {
         LinearLayout listLayout = (LinearLayout)playlistInflater.inflate(R.layout.playlist, parent, false);
         //layout for each individual playlist in the list. Uses playlist.xml
         TextView playlistView = (TextView)listLayout.findViewById(R.id.playlist_name);
+        TextView countView = (TextView)listLayout.findViewById(R.id.playlist_count);
+
         Playlist currentPlaylist = playlistArray.get(position);
         playlistView.setText(currentPlaylist.getTitle()); //pass data to textView objects in each list item
+
+        String songString = new String("");
+        if(playlistArray.get(position).getPlaylistSongs().size() == 1){
+            songString = " song";
+        }else{
+            songString = " songs";
+        }
+        countView.setText(playlistArray.get(position).getPlaylistSongs().size() + songString);
 
         listLayout.setTag(position); //use the playlist's position in list as a tag
         return listLayout;
